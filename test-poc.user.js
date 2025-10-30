7f42b9b977f2f9bcc9c16e5189ecc58329c970fa
ales-marcik <126060808+ales-marcik@users.noreply.github.com>
2025-10-30 21:39:04 +0100

    Update test-poc.user.js

--- test-poc.user.js
+++ test-poc.user.js
@@ -1,12 +1,8 @@
 // ==UserScript==
-// @name         New Userscript - github POC test
-// @namespace    http://tampermonkey.net/
+// @name         github POC test
 // @version      2025-10-30.a
-// @description  try to take over the world!
-// @author       You
 // @match        https://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
 // ==/UserScript==
 
 (function() {

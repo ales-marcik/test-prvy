e3489783aa413e1b22428d2e3f39836318dfc519
ales-marcik <126060808+ales-marcik@users.noreply.github.com>
2025-10-31 13:02:19 +0100

    Update test-poc.user.js

--- test-poc.user.js
+++ test-poc.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name         github POC test
-// @version      2025-10-30.b
-// @match        https://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @version      2025-10-31.a
+// @match        https://dummy
 // ==/UserScript==
 
 (function() {
